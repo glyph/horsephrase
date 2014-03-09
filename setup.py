9bd1fe9dc24e4ef07166a0c977607f3a113730c8
Glyph <glyph@twistedmatrix.com>
2014-03-09 16:57:42 -0700

    Trying to remember how setuptools goes.

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import setup
+
+setup(
+    name="Horsephrase",
+    version="0.1",
+    description="Secure password generator.",
+    long_description=(
+        "Like http://correcthorsebatterystaple.net/ except it's not a web page"
+        " which is logging your passwords and sending them all to the NSA."
+    ),
+    author="Glyph",
+    author_email="glyph@twistedmatrix.com",
+    maintainer="Glyph",
+    maintainer_email="glyph@twistedmatrix.com",
+    url="https://github.com/glyph/Horsephrase/",
+    packages=["horsephrase"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+    ],
+    entry_points={
+        "console_scripts": [
+            "horsephrase = horsephrase:generate",
+        ],
+    },
+)
