9174192742da31a5dc80f5681a51bb0441073995
Glyph <glyph@twistedmatrix.com>
2022-12-08 14:09:13 -0800

    drop py2 support, add type hints, modernize everything

--- horsephrase/_implementation.py
+++ horsephrase/_implementation.py
@@ -1,20 +1,26 @@
-
-from __future__ import absolute_import, unicode_literals, print_function
-
-from pkg_resources import resource_string
+from importlib.resources import files
 
 # See http://jbauman.com/aboutgsl.html
-words = resource_string(__name__, "words.txt").decode("utf-8").split()
+with (files(__name__) / "words.txt").open() as _:
+    words = _.read().strip().split("\n")
+del _
+
 
 from random import SystemRandom
+from typing import TypeVar, Callable, Sequence
 
-def generate(number=4, choice=SystemRandom().choice, words=words, joiner=" "):
+def generate(
+    number: int = 4,
+    choice: Callable[[Sequence[str]], str] = SystemRandom().choice,
+    words: list[str] = words,
+    joiner: str = " ",
+) -> str:
     """
     Generate a random passphrase from the GSL.
     """
     return joiner.join(choice(words) for each in range(number))
 
 
-def output():
+def output() -> int:
     print(generate())
     return 0
